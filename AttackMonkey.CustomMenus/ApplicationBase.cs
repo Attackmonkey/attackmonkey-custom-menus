51e68e59cff42bb6054d842c1023172d01f8ea59
Tim <tim.payne@epiphanysolutions.co.uk>
2014-12-01 23:52:50 +0000

    Updated to v7.

--- AttackMonkey.CustomMenus/ApplicationBase.cs
+++ AttackMonkey.CustomMenus/ApplicationBase.cs
@@ -4,24 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using System.IO;
-using umbraco;
-using umbraco.cms.presentation.Trees;
-using umbraco.cms.businesslogic.web;
-using umbraco.cms.businesslogic.media;
-using umbraco.BasePages;
+using Umbraco.Core;
 using umbraco.interfaces;
 
 namespace AttackMonkey.CustomMenus
 {
-	public class ApplicationBase : umbraco.BusinessLogic.ApplicationBase
+	public class ApplicationBase : IApplicationEventHandler
 	{
-		/// <summary>
-		/// Main methods, sets up config watcher and event listeners
-		/// </summary>
-		public ApplicationBase()
+		public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, Umbraco.Core.ApplicationContext applicationContext)
+		{
+
+		}
+
+		public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+		{
+
+		}
+
+		public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
 		{
 			//set up action listeners
-			BaseTree.BeforeNodeRender += new umbraco.cms.presentation.Trees.BaseTree.BeforeNodeRenderEventHandler(SetCustomMenus);
+			Umbraco.Web.Trees.ContentTreeController.MenuRendering += SetCustomMenus;
+			Umbraco.Web.Trees.ContentTreeController.TreeNodesRendering += ContentTreeController_TreeNodesRendering;
 			
 			//code to expire config if the config file is changed
 			if (HttpContext.Current.Application["customMenus"] == null)
@@ -37,6 +41,65 @@ namespace AttackMonkey.CustomMenus
 			}
 		}
 
+		void ContentTreeController_TreeNodesRendering(Umbraco.Web.Trees.TreeControllerBase sender, Umbraco.Web.Trees.TreeNodesRenderingEventArgs e)
+		{
+			//if they've set the ignore for Admins option, don't parse these rules
+			if (!(Config.Instance.IgnoreForAdmin && umbraco.BusinessLogic.User.GetCurrent().IsAdmin()))
+			{
+				foreach (var node in e.Nodes)
+				{
+					//only call if the node has a menu in the first place
+					if (sender.TreeAlias == "content" || sender.TreeAlias == "media")
+					{
+						string alias = string.Empty;
+						int nodeId = 0;
+						string path = string.Empty;
+
+						ConfigItem config = null;
+
+						if (sender.TreeAlias == "content" && Convert.ToInt32(node.Id) > 0)
+						{
+							//get the document
+							var content = sender.Services.ContentService.GetById(Convert.ToInt32(node.Id));
+
+							alias = content.ContentType.Alias;
+							nodeId = content.Id;
+							path = content.Path;
+						}
+						else if (sender.TreeAlias == "media" && Convert.ToInt32(node.Id) > 0 && Config.Instance.UseInMediaSection)
+						{
+							//get the media item
+							var media = sender.Services.MediaService.GetById(Convert.ToInt32(node.Id));
+
+							alias = media.ContentType.Alias;
+							nodeId = media.Id;
+							path = media.Path;
+						}
+
+						//only carry on if we have a node id greater than 0
+						if (nodeId != 0)
+						{
+							if (Config.Instance.ConfigEntries.Any(o => o.DocTypeAlias == alias))
+							{
+								//doctype match, get the config item
+								config = Config.Instance.ConfigEntries.First(o => o.DocTypeAlias == alias);
+							}
+							else if (Config.Instance.ConfigEntries.Any(o => o.NodeId == nodeId))
+							{
+								//node id match, get the config item
+								config = Config.Instance.ConfigEntries.First(o => o.NodeId == nodeId);
+							}
+
+							if (config != null && !string.IsNullOrEmpty(config.ClickAction))
+							{
+								node.RoutePath = config.ClickAction;
+							}
+						}
+					}
+				}
+			}
+		}
+
 		/// <summary>
 		/// Expires the config for the package
 		/// </summary>
@@ -48,16 +111,17 @@ namespace AttackMonkey.CustomMenus
 		}
 
 		/// <summary>
-		/// New static method to enable it to work properly with CustomContentTree
+		/// Sets the custom menus, based on your config
 		/// </summary>
-		/// <param name="node">the tree node to process</param>
-		public static void ProcessMenu(ref umbraco.cms.presentation.Trees.XmlTreeNode node)
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void SetCustomMenus(Umbraco.Web.Trees.TreeControllerBase sender, Umbraco.Web.Trees.MenuRenderingEventArgs e)
 		{
 			//if they've set the ignore for Admins option, don't parse these rules
 			if (!(Config.Instance.IgnoreForAdmin && umbraco.BusinessLogic.User.GetCurrent().IsAdmin()))
 			{
 				//only call if the node has a menu in the first place
-				if (node.Menu != null && (node.NodeType == "content" || node.NodeType == "media"))
+				if (e.Menu != null && (sender.TreeAlias == "content" || sender.TreeAlias == "media"))
 				{
 					string alias = string.Empty;
 					int nodeId = 0;
@@ -65,22 +129,22 @@ namespace AttackMonkey.CustomMenus
 
 					ConfigItem config = null;
 
-					if (node.NodeType == "content")
+					if (sender.TreeAlias == "content" && Convert.ToInt32(e.NodeId) > 0)
 					{
 						//get the document
-						Document doc = new Document(true, int.Parse(node.NodeID));
+						var content = sender.Services.ContentService.GetById(Convert.ToInt32(e.NodeId));
 
-						alias = doc.ContentType.Alias;
-						nodeId = Convert.ToInt32(node.NodeID);
-						path = doc.Path;
+						alias = content.ContentType.Alias;
+						nodeId = content.Id;
+						path = content.Path;
 					}
-					else if (node.NodeType == "media" && Config.Instance.UseInMediaSection)
+					else if (sender.TreeAlias == "media" && Convert.ToInt32(e.NodeId) > 0 && Config.Instance.UseInMediaSection)
 					{
 						//get the media item
-						Media media = new Media(int.Parse(node.NodeID));
+						var media = sender.Services.MediaService.GetById(int.Parse(e.NodeId));
 
 						alias = media.ContentType.Alias;
-						nodeId = Convert.ToInt32(node.NodeID);
+						nodeId = media.Id;
 						path = media.Path;
 					}
 
@@ -103,53 +167,38 @@ namespace AttackMonkey.CustomMenus
 							//first clear the current menu items, but only if there are things we can do with it
 							if (config.MenuItems.Count > 0)
 							{
-								node.Menu.Clear();
-							}
-
-							//if there's a click action, add it
-							if (!string.IsNullOrEmpty(config.ClickAction))
-							{
-								node.Action = config.ClickAction;
+								e.Menu.Items.Clear();
 							}
 
 							//get the list of actions the user is allowed to do on this node.
-							List<IAction> allowedActions = umbraco.BusinessLogic.Actions.Action.FromString(UmbracoEnsuredPage.CurrentUser.GetPermissions(path));
+							List<IAction> allowedActions = umbraco.BusinessLogic.Actions.Action.FromString(umbraco.BusinessLogic.User.GetCurrent().GetPermissions(path));
 
 							//now loop through and add the items from the list, checking that the current user has permissions for the item in the first place
 							foreach (var action in config.MenuItems)
 							{
 								if (!action.CanBePermissionAssigned || (action.CanBePermissionAssigned && allowedActions.Contains(action)))
 								{
-									node.Menu.Add(action);
+									e.Menu.Items.Add(new Umbraco.Web.Models.Trees.MenuItem(action));
 								}
 							}
 
 							//loop through the remove menu items and remove them
 							foreach (var action in config.RemoveMenuItems)
 							{
-								if (node.Menu.Any(a => a.Alias == action.Alias))
+								if (e.Menu.Items.Any(a => a.Alias == action.Alias))
 								{
-									node.Menu.Remove(node.Menu.First(a => a.Alias == action.Alias && a.JsFunctionName.Contains("RelationType") == false));
+									e.Menu.Items.Remove(e.Menu.Items.First(a => a.Alias == action.Alias));
 								}
 							}
 
-							//experimental code for fixing URL issue
-							
+							if (config.RemoveMenuItems.Any(a => a.Alias == e.Menu.DefaultMenuAlias) || !string.IsNullOrEmpty(config.ClickAction))
+							{
+								e.Menu.DefaultMenuAlias = string.Empty;
+							}
 						}
 					}
 				}
 			}
-		}
-
-		/// <summary>
-		/// Sets the custom menus, based on your config
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="node"></param>
-		/// <param name="e"></param>
-		void SetCustomMenus(ref umbraco.cms.presentation.Trees.XmlTree sender, ref umbraco.cms.presentation.Trees.XmlTreeNode node, EventArgs e)
-		{
-			ProcessMenu(ref node);
 		}
 	}
 }
